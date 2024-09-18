d38227736677533a530e0e93f382df4db7704db3
michele gregori <michelegregorits@gmail.com>
2024-09-18 22:29:25 +0200

    clock source

--- async_sources/timer.py
+++ async_sources/timer.py
@@ -1,6 +1,6 @@
 from .source import Source
 from asyncio import sleep
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 from typing import List
 
 class TimerSource(Source):
@@ -24,4 +24,26 @@ class TimerSource(Source):
             await sleep(self.period_seconds)
 
         return [datetime.now(self.timezone)]
+
+
+class ClockSource(Source):
+
+    def __init__(self, period_seconds: float, timezone = timezone.utc, feeding_subscriptions_policy: str = 'on_subscribe'):
+
+        self.period_seconds = period_seconds
+        self.timezone = timezone
+        self._to_be_replaced = {'microsecond':0}
+        if period_seconds // 60 > 0:
+            self._to_be_replaced['second'] = 0
+        if period_seconds // (60*60) > 0:
+            self._to_be_replaced['minute'] = 0
+
+        super().__init__(feeding_subscriptions_policy=feeding_subscriptions_policy)
+
+    async def _process_update(self, *args) -> List[datetime]:
+
+        now = datetime.now(self.timezone)
+        then = now.replace(**self._to_be_replaced) + timedelta(seconds=self.period_seconds)
+        await sleep((then-now).total_seconds())
+        return [then]
 
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,5 +1,5 @@
 from .fixtures import *
-from async_sources.timer import TimerSource
+from async_sources.timer import TimerSource, ClockSource
 from datetime import datetime
 import pytest
 
@@ -17,3 +17,21 @@ async def test_timer():
     tock = datetime.now()
 
     assert abs((tock-tick).total_seconds() - period) < 0.1
+
+@pytest.mark.asyncio
+async def test_clock():
+
+    period = 1
+    source = ClockSource(period, feeding_subscriptions_policy='immediate')
+    subscription = source.subscribe()
+
+    tick=(await subscription.get()).data
+    real_tick = datetime.now()
+    tock =(await subscription.get()).data
+    real_tock = datetime.now()
+
+    assert abs((real_tock-real_tick).total_seconds() - period) < 0.1
+    assert tick.microsecond == 0
+    assert tock.microsecond == 0
+    assert tick.tzinfo == source.timezone
+    assert tock.tzinfo == source.timezone
